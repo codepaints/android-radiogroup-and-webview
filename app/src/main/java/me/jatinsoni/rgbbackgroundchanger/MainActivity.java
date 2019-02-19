7c02a25edc3339c4e23eb0f0172f723b8f34220b
Jatin Soni <pixelngrain@gmail.com>
2019-02-19 14:37:42 +0400

    Moved radio implement method separately

--- app/src/main/java/me/jatinsoni/rgbbackgroundchanger/MainActivity.java
+++ app/src/main/java/me/jatinsoni/rgbbackgroundchanger/MainActivity.java
@@ -17,7 +17,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
 
     ConstraintLayout parentLayout;
     RadioGroup radioGroup;
@@ -52,69 +52,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         google.setOnClickListener(this);
         yahoo.setOnClickListener(this);
-
+        radioGroup.setOnCheckedChangeListener(this);
 
         // set on radio item change listener in the radio group
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                // get the checked radio button id from the radio group
-                checkedColor = group.findViewById(checkedId);
-                // set the boolean if checked
-                boolean isChecked = checkedColor.isChecked();
-
-                // cross check if the radio is checked
-                if (isChecked) {
-
-                    // convert the radio button text to string
-                    String color = checkedColor.getText().toString();
-
-                    // switch the color based on the radio button checked
-                    switch (color) {
-
-                        //if checked red
-                        case "Red":
-                            //set status bar color
-                            window.setStatusBarColor(getResources().getColor(R.color.colorRedDark));
-                            // set action bar color
-                            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorRed)));
-                            //set the parent layout background red
-                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgRed));
-                            break;
-
-                        //if checked green
-                        case "Green":
-                            //set status bar color
-                            window.setStatusBarColor(getResources().getColor(R.color.colorGreenDark));
-                            // set action bar color
-                            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorGreen)));
-                            //set the parent layout background green
-                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgGreen));
-                            break;
-
-                        //if checked blue
-                        case "Blue":
-                            //set status bar color
-                            window.setStatusBarColor(getResources().getColor(R.color.colorBlueDark));
-                            // set action bar color
-                            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorBlue)));
-                            //set the parent layout background blue
-                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgBlue));
-                            break;
-
-                        default:
-                            //set the parent layout background default (white)
-                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgDefault));
-                            break;
-                    }
-
-                    // display toast for the selected radio button text
-                    Toast.makeText(MainActivity.this, "Checked: " + checkedColor.getText(), Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//
+//                // get the checked radio button id from the radio group
+//                checkedColor = group.findViewById(checkedId);
+//                // set the boolean if checked
+//                boolean isChecked = checkedColor.isChecked();
+//
+//                // cross check if the radio is checked
+//                if (isChecked) {
+//
+//                    // convert the radio button text to string
+//                    String color = checkedColor.getText().toString();
+//
+//                    // switch the color based on the radio button checked
+//                    switch (color) {
+//
+//                        //if checked red
+//                        case "Red":
+//                            //set status bar color
+//                            window.setStatusBarColor(getResources().getColor(R.color.colorRedDark));
+//                            // set action bar color
+//                            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorRed)));
+//                            //set the parent layout background red
+//                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgRed));
+//                            break;
+//
+//                        //if checked green
+//                        case "Green":
+//                            //set status bar color
+//                            window.setStatusBarColor(getResources().getColor(R.color.colorGreenDark));
+//                            // set action bar color
+//                            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorGreen)));
+//                            //set the parent layout background green
+//                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgGreen));
+//                            break;
+//
+//                        //if checked blue
+//                        case "Blue":
+//                            //set status bar color
+//                            window.setStatusBarColor(getResources().getColor(R.color.colorBlueDark));
+//                            // set action bar color
+//                            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorBlue)));
+//                            //set the parent layout background blue
+//                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgBlue));
+//                            break;
+//
+//                        default:
+//                            //set the parent layout background default (white)
+//                            parentLayout.setBackgroundColor(getResources().getColor(R.color.bgDefault));
+//                            break;
+//                    }
+//
+//                    // display toast for the selected radio button text
+//                    Toast.makeText(MainActivity.this, "Checked: " + checkedColor.getText(), Toast.LENGTH_SHORT).show();
+//                }
+//
+//            }
+//        });
 
     }
 
@@ -140,6 +140,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         webView.loadUrl(webUrl);
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+        // get the checked radio button id from the radio group
+        checkedColor = group.findViewById(checkedId);
+        // set the boolean if checked
+        boolean isChecked = checkedColor.isChecked();
+
+        // cross check if the radio is checked
+        if (isChecked) {
+
+            // convert the radio button text to string
+            String color = checkedColor.getText().toString();
+
+            // switch the color based on the radio button checked
+            switch (color) {
+
+                //if checked red
+                case "Red":
+                    //set status bar color
+                    window.setStatusBarColor(getResources().getColor(R.color.colorRedDark));
+                    // set action bar color
+                    actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorRed)));
+                    //set the parent layout background red
+                    parentLayout.setBackgroundColor(getResources().getColor(R.color.bgRed));
+                    break;
+
+                //if checked green
+                case "Green":
+                    //set status bar color
+                    window.setStatusBarColor(getResources().getColor(R.color.colorGreenDark));
+                    // set action bar color
+                    actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorGreen)));
+                    //set the parent layout background green
+                    parentLayout.setBackgroundColor(getResources().getColor(R.color.bgGreen));
+                    break;
+
+                //if checked blue
+                case "Blue":
+                    //set status bar color
+                    window.setStatusBarColor(getResources().getColor(R.color.colorBlueDark));
+                    // set action bar color
+                    actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorBlue)));
+                    //set the parent layout background blue
+                    parentLayout.setBackgroundColor(getResources().getColor(R.color.bgBlue));
+                    break;
+
+                default:
+                    //set the parent layout background default (white)
+                    parentLayout.setBackgroundColor(getResources().getColor(R.color.bgDefault));
+                    break;
+            }
+
+            // display toast for the selected radio button text
+            Toast.makeText(MainActivity.this, "Checked: " + checkedColor.getText(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
